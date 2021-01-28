8ab494bc6869c6779632eab6eb3dbdd966009548
Shawn Chumbar <schumbar@ucsc.edu>
2021-01-28 01:06:36 -0800

    Added assignment details for ICE 01

--- ICEs/ICE01/script.js
+++ ICEs/ICE01/script.js
@@ -1,1 +1,5 @@
+/*global $ */
+// We need to add the line above to get rid of the following error: '$' was used beofre it was defined.
+
+// Shawn Chumbar
 $("#content").append("<strong>JavaScript is working!</strong>");
