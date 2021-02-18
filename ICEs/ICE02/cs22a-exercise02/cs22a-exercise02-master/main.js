1b7a4696b64f709ab4a031e0a45b6c5d4f6cfd78
Shawn Chumbar <schumbar@ucsc.edu>
2021-02-18 01:28:27 -0800

    Adding edited main.js file to repo. Need to doublecheck if the console should be printing out the getPrice function and assosciated objects.

--- ICEs/ICE02/cs22a-exercise02/cs22a-exercise02-master/main.js
+++ ICEs/ICE02/cs22a-exercise02/cs22a-exercise02-master/main.js
@@ -3,8 +3,25 @@
 var main = function () {
     "use strict";
     // TODO: Put all your code BELOW this line in this 'main' function
+    var getPrice = function () {
+        var the_price = 500;
+        the_price += (this.speed === '4GHz') ? 300 : 100;
+        the_price += (this.hdspace === '1TB') ? 150 : 80;
+        the_price += (this.ram === '16GB') ? 200 : 100;
+        return the_price;
+    };
 
+    var Computer = function (speed, hdspace, ram) {
+        this.speed = speed;
+        this.hdspace = hdspace;
+        this.ram = ram;
+    };
+    Computer.prototype.price = getPrice;
 
+    var workComputer = new Computer('3.4GHz', '500GB', '4GB');
+    var homeComputer = new Computer('4GHz', '1TB', '16GB');
+    var laptop = new Computer('2.93GHz', '250GB', '8GB');
+    
     // TODO: Put all your code ABOVE this line in this 'main' function
     // Build an object/hash table/associative array of the Computer objects you were supposed to create
     // with user fiendly key names using an object literal.
